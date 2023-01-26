ddd008e242195652c41d29a287fded141d398e1b
C. Frank Miller <95247698+cfrankmiller@users.noreply.github.com>
2023-01-26 14:08:12 -0800

    Add doc and scripts to build for runtimecore (#1)

---
+++ esri/build_opencv_linux.sh
@@ -0,0 +1,61 @@
+
+set -ex
+
+rm -rf build
+rm -rf install
+
+mkdir build
+cd build
+cmake \
+  -GNinja \
+  -DCMAKE_C_COMPILER=/usr/local/rtc/llvm/11.0.0/bin/clang \
+  -DCMAKE_CXX_COMPILER=/usr/local/rtc/llvm/11.0.0/bin/clang++ \
+  -DCMAKE_C_FLAGS="-m64" \
+  -DCMAKE_CXX_FLAGS="-m64 -stdlib=libc++" \
+  -DCMAKE_SHARED_LINKER_FLAGS="-stdlib=libc++ -static-libstdc++ -fuse-ld=lld -rtlib=compiler-rt -ldl -pthread" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=../install \
+  -DENABLE_THIN_LTO=TRUE \
+  -DOPENCV_PYTHON_SKIP_DETECTION=ON \
+  -DWITH_FFMPEG=OFF \
+  -DWITH_GTK=OFF \
+  -DWITH_OPENCL=OFF \
+  -DWITH_JPEG=OFF \
+  -DWITH_JASPER=OFF \
+  -DWITH_IPP=OFF \
+  -DWITH_OPENEXR=OFF \
+  -DWITH_OPENJPEG=OFF \
+  -DWITH_PNG=OFF \
+  -DWITH_PROTOBUF=OFF \
+  -DWITH_TIFF=OFF \
+  -DWITH_WEBP=OFF \
+  -DBUILD_ZLIB=ON \
+  -DBUILD_opencv_apps=OFF \
+  -DBUILD_opencv_features2d=OFF \
+  -DBUILD_opencv_flann=OFF \
+  -DBUILD_opencv_core=ON \
+  -DBUILD_opencv_imgcodecs=OFF \
+  -DBUILD_opencv_imgproc=ON \
+  -DBUILD_opencv_apps=OFF \
+  -DBUILD_opencv_calib3d=OFF \
+  -DBUILD_opencv_dnn=OFF \
+  -DBUILD_opencv_features2d=OFF \
+  -DBUILD_opencv_flann=OFF \
+  -DBUILD_opencv_gapi=OFF \
+  -DBUILD_opencv_highgui=OFF \
+  -DBUILD_opencv_java_bindings_generator=OFF \
+  -DBUILD_opencv_js=OFF \
+  -DBUILD_opencv_js_bindings_generator=OFF \
+  -DBUILD_opencv_ml=OFF \
+  -DBUILD_opencv_objc_bindings_generator=OFF \
+  -DBUILD_opencv_objdetect=OFF \
+  -DBUILD_opencv_photo=OFF \
+  -DBUILD_opencv_python_bindings_generator=OFF \
+  -DBUILD_opencv_python_tests=OFF \
+  -DBUILD_opencv_stitching=OFF \
+  -DBUILD_opencv_ts=OFF \
+  -DBUILD_opencv_video=OFF \
+  -DBUILD_opencv_videoio=OFF \
+  -DBUILD_opencv_world=OFF \
+  ../..
+ninja -v install
---
+++ esri/build_opencv_macos.sh
@@ -0,0 +1,60 @@
+
+set -ex
+
+rm -rf build
+rm -rf install
+
+mkdir build
+cd build
+
+export DEVELOPER_DIR="/Applications/Xcode_13.2.1.app/Contents/Developer"
+export CXX="/usr/bin/clang++"
+PATH="/usr/local/rtc/cmake/3.22.0/bin:/usr/local/rtc/ninja/1.10.2/bin:$PATH"
+
+cmake -S ../.. -B build \
+  -GNinja \
+  -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=install \
+  -DOPENCV_PYTHON_SKIP_DETECTION=ON \
+  -DWITH_FFMPEG=OFF \
+  -DWITH_GTK=OFF \
+  -DWITH_OPENCL=OFF \
+  -DWITH_JPEG=OFF \
+  -DWITH_JASPER=OFF \
+  -DWITH_IPP=OFF \
+  -DWITH_OPENEXR=OFF \
+  -DWITH_OPENJPEG=OFF \
+  -DWITH_PNG=OFF \
+  -DWITH_PROTOBUF=OFF \
+  -DWITH_TIFF=OFF \
+  -DWITH_WEBP=OFF \
+  -DBUILD_ZLIB=ON \
+  -DBUILD_opencv_apps=OFF \
+  -DBUILD_opencv_features2d=OFF \
+  -DBUILD_opencv_flann=OFF \
+  -DBUILD_opencv_core=ON \
+  -DBUILD_opencv_imgcodecs=OFF \
+  -DBUILD_opencv_imgproc=ON \
+  -DBUILD_opencv_apps=OFF \
+  -DBUILD_opencv_calib3d=OFF \
+  -DBUILD_opencv_dnn=OFF \
+  -DBUILD_opencv_features2d=OFF \
+  -DBUILD_opencv_flann=OFF \
+  -DBUILD_opencv_gapi=OFF \
+  -DBUILD_opencv_highgui=OFF \
+  -DBUILD_opencv_java_bindings_generator=OFF \
+  -DBUILD_opencv_js=OFF \
+  -DBUILD_opencv_js_bindings_generator=OFF \
+  -DBUILD_opencv_ml=OFF \
+  -DBUILD_opencv_objc_bindings_generator=OFF \
+  -DBUILD_opencv_objdetect=OFF \
+  -DBUILD_opencv_photo=OFF \
+  -DBUILD_opencv_python_bindings_generator=OFF \
+  -DBUILD_opencv_python_tests=OFF \
+  -DBUILD_opencv_stitching=OFF \
+  -DBUILD_opencv_ts=OFF \
+  -DBUILD_opencv_video=OFF \
+  -DBUILD_opencv_videoio=OFF \
+  -DBUILD_opencv_world=OFF
+cmake --build build -t install
